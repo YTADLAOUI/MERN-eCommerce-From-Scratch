338643234ebbee05af3874ae0ea9aa4a168d9866
youssef tadlaoui <usftadlaoui@gmail.com>
2023-11-10 12:43:06 +0100

    data_users

---
+++ back_end/src/database/users.js
@@ -0,0 +1,20 @@
+const bcrypt=require('bcryptjs')
+const users=[
+  {
+    name:'Admin User',
+    email:'admin@exemple.com',
+    password: bcrypt.hashSync('123456',10),
+    isAdmin:true,
+  },
+  {
+    name:'John Doe',
+    email:'john@exemple.com',
+    password: bcrypt.hashSync('123456',10),
+  },
+  {
+    name:'Jane Doe',
+    email:'jane@exemple.com',
+    password: bcrypt.hashSync('123456',10),
+  },
+]
+export default users;
